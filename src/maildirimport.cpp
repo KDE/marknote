a3abcbb1d2ccaeb854059cecdae074914d7f5a76
Carl Schwan <carl@carlschwan.eu>
2024-06-16 15:25:50 +0200

    Fix file creation on windows

--- src/maildirimport.cpp
+++ src/maildirimport.cpp
@@ -17,6 +17,42 @@ MaildirImport::MaildirImport(QObject *parent)
 {
 }
 
+static QString cleanFileName(const QString &name)
+{
+    QString fileName = name.trimmed();
+
+    // We need to replace colons with underscores since those cause problems with
+    // KFileDialog (bug in KFileDialog though) and also on Windows filesystems.
+    // We also look at the special case of ": ", since converting that to "_ "
+    // would look strange, simply "_" looks better.
+    // https://issues.kolab.org/issue3805
+    fileName.replace(QLatin1StringView(": "), QStringLiteral("_"));
+    // replace all ':' with '_' because ':' isn't allowed on FAT volumes
+    fileName.replace(QLatin1Char(':'), QLatin1Char('_'));
+    // better not use a dir-delimiter in a filename
+    fileName.replace(QLatin1Char('/'), QLatin1Char('_'));
+    fileName.replace(QLatin1Char('\\'), QLatin1Char('_'));
+
+#ifdef Q_OS_WINDOWS
+    // replace all '.' with '_', not just at the start of the filename
+    // but don't replace the last '.' before the file extension.
+    int i = fileName.lastIndexOf(QLatin1Char('.'));
+    if (i != -1) {
+        i = fileName.lastIndexOf(QLatin1Char('.'), i - 1);
+    }
+
+    while (i != -1) {
+        fileName.replace(i, 1, QLatin1Char('_'));
+        i = fileName.lastIndexOf(QLatin1Char('.'), i - 1);
+    }
+#endif
+
+    // replace all '~' with '_', not just leading '~' either.
+    fileName.replace(QLatin1Char('~'), QLatin1Char('_'));
+
+    return fileName;
+}
+
 void MaildirImport::import(const QUrl &maildir, const QUrl &destinationDir)
 {
     QFileInfo maildirInfo(maildir.toLocalFile());
@@ -46,7 +82,7 @@ void MaildirImport::import(const QUrl &maildir, const QUrl &destinationDir)
                 const auto subject = msg->subject()->asUnicodeString();
                 const auto content = msg->decodedContent();
 
-                QFile markdownFile(destinationDir.toLocalFile() + u'/' + QString::fromUtf8(QFile::encodeName(subject)) + u".md"_s);
+                QFile markdownFile(destinationDir.toLocalFile() + u'/' + cleanFileName(subject) + u".md"_s);
                 if (!markdownFile.open(QIODevice::WriteOnly)) {
                     qWarning() << "Not writable" << markdownFile.fileName() << markdownFile.errorString();
                     Q_EMIT errorOccurred(i18nc("@status", "An error occurred while writing to '%1'", markdownFile.fileName()));
