KDE/marknote

commit 813b9566aa7e38027621c7dc400390031307960b

qmllint: Fix warnings
- Remove unused imports
- Unqualified access
- Missing property
- Detected height on an item that is managed by a layout. This is undefined behavior; use implictHeight or Layout.preferredHeight instead. [Quick.layout-positioning]
- Added required property for sorting
- Some other misc warnings not listed before

src/qml/NotesPage.qml
@@ -5,22 +5,23 @@
 import QtQuick
 import QtQuick.Controls
 import QtQuick.Layouts
-import QtQuick.Templates as T
 import QtQuick.Dialogs
 import org.kde.kitemmodels
 import org.kde.marknote
 import org.kde.notification
+import org.kde.ki18n
 import org.kde.kirigami as Kirigami
 import org.kde.kirigamiaddons.delegates as Delegates
 import org.kde.kirigamiaddons.components as Components
-import org.kde.kirigamiaddons.formcard as FormCard
+
+pragma ComponentBehavior: Bound
 
 import "components"
 
 Kirigami.ScrollablePage {
     id: root
 
-    readonly property bool isWideScreen: ApplicationWindow.window ? ApplicationWindow.window.wideScreen : false
+    readonly property bool isWideScreen: !!ApplicationWindow.window?.isWideScreen // qmllint disable missing-property
 
     objectName: "NotesPage"
     property color backgroundColor: Kirigami.ColorUtils.linearInterpolation(Kirigami.Theme.backgroundColor, Kirigami.Theme.alternateBackgroundColor, 0.6)
@@ -36,9 +37,14 @@ Kirigami.ScrollablePage {
             bottomMargin: Kirigami.Units.gridUnit
 
         }
-        text: i18n("Add note")
+        text: KI18n.i18n("Add note")
         icon.name: "list-add"
         onClicked: newNoteAction.trigger()
+    }
+
+    enum SortRole {
+        SortName,
+        SortDate
     }
 
     Connections {
@@ -177,7 +183,7 @@ Kirigami.ScrollablePage {
         ToolButton {
             id: searchButton
             icon.name: titleLayout.searchOpen ? "draw-arrow-back" : "search"
-            text: titleLayout.searchOpen ? i18n("Exit Search (%1)", cancelShortcut.nativeText) : i18n("Search notes (%1)", searchShortcut.nativeText)
+            text: titleLayout.searchOpen ? KI18n.i18n("Exit Search (%1)", cancelShortcut.nativeText) : KI18n.i18n("Search notes (%1)", searchShortcut.nativeText)
             display: AbstractButton.IconOnly
 
             ToolTip.delay: Kirigami.Units.toolTipDelay
@@ -223,7 +229,7 @@ Kirigami.ScrollablePage {
         ToolButton {
             visible: ApplicationWindow.window ? (ApplicationWindow.window.visibility === Window.FullScreen && ApplicationWindow.window.pageStack.depth !== 2) : false
             icon.name: "window-restore-symbolic"
-            text: i18nc("@action:menu", "Exit Full Screen")
+            text: KI18n.i18nc("@action:menu", "Exit Full Screen")
             display: AbstractButton.IconOnly
             checkable: true
             checked: true
@@ -243,7 +249,7 @@ Kirigami.ScrollablePage {
         property string noteName
 
         dialogType: Components.MessageDialog.Warning
-        title: i18nc("@title:window", "Delete Note")
+        title: KI18n.i18nc("@title:window", "Delete Note")
         onRejected: close()
         onAccepted: {
             notesModel.deleteNote(fileUrl);
@@ -253,16 +259,12 @@ Kirigami.ScrollablePage {
             close();
         }
         standardButtons: Dialog.Ok | Dialog.Cancel
-        subtitle: i18n("Are you sure you want to delete the note <b> %1 </b>? This will delete the file <b>%2</b> definitively.", removeDialog.noteName, removeDialog.notePath)
+        subtitle: KI18n.i18n("Are you sure you want to delete the note <b> %1 </b>? This will delete the file <b>%2</b> definitively.", removeDialog.noteName, removeDialog.notePath)
     }
 
     ListView {
         id: notesList
 
-        enum SortRole {
-            SortName,
-            SortDate
-        }
         currentIndex: -1
 
         populate: Transition {
@@ -322,7 +324,9 @@ Kirigami.ScrollablePage {
         Component {
             id: sectionDelegate
             Kirigami.ListSectionHeader {
-                label: section
+                required property string section
+
+                text: section
                 width: parent.width
             }
         }
@@ -333,19 +337,16 @@ Kirigami.ScrollablePage {
             Item {}
         }
 
-        state: Config.sortBehaviour
         states: [
             State {
                 name: "sort-name"
 
                 PropertyChanges {
-                    target: filterModel
-                    sortRole: NotesModel.Name
-                    sortOrder: Qt.AscendingOrder
+                    filterModel.sortRole: NotesModel.Name
+                    filterModel.sortOrder: Qt.AscendingOrder
                 }
                 PropertyChanges {
-                    target: notesList.section
-                    delegate: nullComponent
+                    notesList.section.delegate: nullComponent
                 }
                 StateChangeScript {
                     script: filterModel.sort(0, Qt.AscendingOrder)
@@ -355,13 +356,11 @@ Kirigami.ScrollablePage {
                 name: "sort-date"
 
                 PropertyChanges {
-                    target: filterModel
-                    sortRole: NotesModel.Date
-                    sortOrder: Qt.DescendingOrder
+                    filterModel.sortRole: NotesModel.Date
+                    filterModel.sortOrder: Qt.DescendingOrder
                 }
                 PropertyChanges {
-                    target: notesList.section
-                    delegate: sectionDelegate
+                    notesList.section.delegate: sectionDelegate
                 }
                 StateChangeScript {
                     script: filterModel.sort(0, Qt.DescendingOrder)
@@ -390,7 +389,9 @@ Kirigami.ScrollablePage {
         section {
             property: "month"
             delegate: Kirigami.ListSectionHeader {
-                label: section
+                required property string section
+
+                text: section
                 width: parent.width
             }
         }
@@ -443,8 +444,8 @@ Kirigami.ScrollablePage {
 
                     componentName: "marknote"
                     eventId: "exportSuccessful"
-                    title: i18nc("@title:window", "Marknote")
-                    text: i18nc("@info", "Export of \"%1\" was successful.", exportSuccessNotification.name);
+                    title: KI18n.i18nc("@title:window", "Marknote")
+                    text: KI18n.i18nc("@info", "Export of \"%1\" was successful.", exportSuccessNotification.name);
                     iconName: {
                         const ext = exportSuccessNotification.path.split('.').pop().toLowerCase();
 
@@ -462,7 +463,7 @@ Kirigami.ScrollablePage {
                     }
                     actions: [
                         NotificationAction {
-                            label: i18nc("@action:notifaction","Open File")
+                            label: KI18n.i18nc("@action:notifaction","Open File")
                             onActivated: Qt.openUrlExternally(exportSuccessNotification.path)
                         }
                     ]
@@ -479,8 +480,8 @@ Kirigami.ScrollablePage {
 
                     componentName: "marknote"
                     eventId: "exportFailed"
-                    title: i18nc("@title:window", "Marknote")
-                    text: i18nc("@info", "Export of \"%1\" failed.", exportFailedNotification.name);
+                    title: KI18n.i18nc("@title:window", "Marknote")
+                    text: KI18n.i18nc("@info", "Export of \"%1\" failed.", exportFailedNotification.name);
                     iconName: "error"
                 }
             }
@@ -492,7 +493,7 @@ Kirigami.ScrollablePage {
             property Delegates.RoundedItemDelegate delegateItem
 
             Action {
-                text: i18nc("@action:inmenu", "Rename")
+                text: KI18n.i18nc("@action:inmenu", "Rename")
                 icon.name: "document-edit"
                 onTriggered:
                 {
@@ -504,7 +505,7 @@ Kirigami.ScrollablePage {
             }
 
             Action {
-                text: i18nc("@action:inmenu", "Copy")
+                text: KI18n.i18nc("@action:inmenu", "Copy")
                 icon.name: "edit-copy"
                 onTriggered: {
                     notesModel.copyWholeNote(menu.delegateItem.fileUrl)
@@ -523,7 +524,7 @@ Kirigami.ScrollablePage {
             }
 
             Action {
-                text: i18nc("@action:inmenu", "Duplicate")
+                text: KI18n.i18nc("@action:inmenu", "Duplicate")
                 icon.name: "edit-duplicate-symbolic"
                 onTriggered: {
                     notesModel.duplicateNote(menu.delegateItem.fileUrl)
@@ -531,31 +532,31 @@ Kirigami.ScrollablePage {
             }
 
             Kirigami.Action {
-                text: i18nc("@action:inmenu", "Export")
+                text: KI18n.i18nc("@action:inmenu", "Export")
                 icon.name: "document-export"
 
                 Kirigami.Action {
-                    text: i18nc("@action:inmenu", "Export to HTML")
+                    text: KI18n.i18nc("@action:inmenu", "Export to HTML")
                     icon.name: "text-html"
                     onTriggered: {
                         fileDialog.name = menu.delegateItem.name;
                         fileDialog.path = menu.delegateItem.fileUrl;
                         fileDialog.selectedFile = menu.delegateItem.name + '.html';
-                        fileDialog.title = i18nc("@title:window", "Export to HTML");
-                        fileDialog.nameFilters = [i18n("HTML file (*.html)")];
+                        fileDialog.title = KI18n.i18nc("@title:window", "Export to HTML");
+                        fileDialog.nameFilters = [KI18n.i18n("HTML file (*.html)")];
                         fileDialog.open();
                     }
                 }
 
                 Kirigami.Action {
-                    text: i18nc("@action:inmenu", "Export to PDF")
+                    text: KI18n.i18nc("@action:inmenu", "Export to PDF")
                     icon.name: "application-pdf"
                     onTriggered: {
                         fileDialog.name = menu.delegateItem.name;
                         fileDialog.path = menu.delegateItem.fileUrl;
                         fileDialog.selectedFile = menu.delegateItem.name + '.pdf';
-                        fileDialog.title = i18nc("@title:window", "Export to PDF");
-                        fileDialog.nameFilters = [i18n("PDF file (*.pdf)")];
+                        fileDialog.title = KI18n.i18nc("@title:window", "Export to PDF");
+                        fileDialog.nameFilters = [KI18n.i18n("PDF file (*.pdf)")];
                         fileDialog.open();
                     }
                 }
@@ -567,8 +568,8 @@ Kirigami.ScrollablePage {
                         fileDialog.name = menu.delegateItem.name;
                         fileDialog.path = menu.delegateItem.fileUrl;
                         fileDialog.selectedFile = menu.delegateItem.name + '.odt';
-                        fileDialog.title = i18nc("@title:window", "Export to ODT");
-                        fileDialog.nameFilters = [i18n("ODF Text Document (*.odt)")];
+                        fileDialog.title = KI18n.i18nc("@title:window", "Export to ODT");
+                        fileDialog.nameFilters = [KI18n.i18n("ODF Text Document (*.odt)")];
                         fileDialog.open();
                     }
                 }
@@ -579,7 +580,7 @@ Kirigami.ScrollablePage {
             }
 
             Action {
-                text: i18nc("@action:inmenu", "Delete")
+                text: KI18n.i18nc("@action:inmenu", "Delete")
                 icon.name: "delete"
                 onTriggered: {
                     removeDialog.noteName = menu.delegateItem.name;
@@ -727,7 +728,7 @@ Kirigami.ScrollablePage {
 
                     Label {
                         Layout.leftMargin: Kirigami.Units.smallSpacing
-                        text: Qt.formatDateTime(date, Qt.SystemLocaleDate)
+                        text: Qt.formatDateTime(delegateItem.date)
                         font: Kirigami.Theme.smallFont
                         color: Kirigami.Theme.disabledTextColor
                         Layout.fillWidth: true
@@ -738,7 +739,7 @@ Kirigami.ScrollablePage {
                 }
 
                 ToolButton{
-                    text: i18nc("@action:button", "Show Menu")
+                    text: KI18n.i18nc("@action:button", "Show Menu")
                     icon.name: "overflow-menu"
                     down: pressed || (menu.opened && menu.delegateItem === delegateItem)
                     display: ToolButton.IconOnly
@@ -782,7 +783,7 @@ Kirigami.ScrollablePage {
             width: parent.width - (Kirigami.Units.largeSpacing * 4)
             icon.name: "note"
             visible: notesList.count === 0
-            text: i18n("Add a note!")
+            text: KI18n.i18n("Add a note!")
             helpfulAction: newNoteAction
         }
     }

CMakeLists.txt
@@ -13,7 +13,7 @@ project(marknote VERSION ${RELEASE_SERVICE_VERSION})
 include(FeatureSummary)
 
 set(QT_MIN_VERSION 6.6)
-set(KF_MIN_VERSION 6.8)
+set(KF_MIN_VERSION 6.21)
 set(KPIM_MIME_VERSION "6.1.0")
 
 find_package(ECM ${KF_MIN_VERSION} REQUIRED NO_MODULE)

src/qml/EditPage.qml
@@ -12,14 +12,12 @@ import QtQuick.Templates as T
 import QtQuick.Layouts
 import org.kde.ki18n
 
-import "components"
-
 import org.kde.marknote
 
 Kirigami.Page {
     id: root
 
-    readonly property bool isWideScreen: !!ApplicationWindow.window?.isWideScreen
+    readonly property bool isWideScreen: !!ApplicationWindow.window?.isWideScreen // qmllint disable missing-property
 
     property bool init: false
 
@@ -29,6 +27,7 @@ Kirigami.Page {
     property string oldPath: ''
     property bool saved: true
     property bool singleDocumentMode: false
+    property bool canFitToolbar: true
     property real dynamicRightPadding: 0
 
     readonly property alias textArea: textArea
@@ -36,6 +35,7 @@ Kirigami.Page {
     required property var document
     readonly property alias contentScroll: contentScroll
     property alias searchBar: searchBar 
+    property TocDrawer tocDrawer: null
     readonly property Kirigami.PageRow pageStack: (root.ApplicationWindow.window as Kirigami.ApplicationWindow)?.pageStack ?? null
     required property TextFieldContextMenu textFieldContextMenu
 
@@ -50,10 +50,20 @@ Kirigami.Page {
         // 30 grid units gives enough room for the 15-unit drawer + 15 units of text
         // Have nothing to do in the source mode
         if (!NavigationController.sourceMode &&
-            tocDrawer.opened &&
-            width < (tocDrawer.width + Kirigami.Units.gridUnit * 15)) {
-            tocDrawer.close()
+            root.tocDrawer && root.tocDrawer.opened &&
+            width < (root.tocDrawer.width + Kirigami.Units.gridUnit * 15)) {
+            root.tocDrawer.close()
             }
+    }
+
+    function toggleToc(): void {
+        if (root.tocDrawer) {
+            if (root.tocDrawer.opened) {
+                root.tocDrawer.close();
+            } else {
+                root.tocDrawer.open();
+            }
+        }
     }
 
     function openSearch(): void {
@@ -214,7 +224,7 @@ Kirigami.Page {
             checkable: true
             checked: root.searchBar.isSearchOpen
 
-            onClicked: toggleSearch()
+            onClicked: root.toggleSearch()
 
             ToolTip.text: KI18n.i18nc("@info:tooltip", "Search in Note")
             ToolTip.visible: hovered
@@ -294,7 +304,7 @@ Kirigami.Page {
             display: AbstractButton.IconOnly
 
             onClicked: {
-                document.saveAs(root.noteFullPath)
+                root.document.saveAs(root.noteFullPath)
                 NavigationController.sourceMode = !NavigationController.sourceMode
             }
         }
@@ -306,7 +316,7 @@ Kirigami.Page {
         }
 
         RowLayout {
-            visible: root.tocPosition > 0 && !root.isNarrow && root.pageStack.columnView.columnResizeMode === Kirigami.ColumnView.FixedColumns
+            visible: root.tocPosition > 0 && !root.canFitToolbar && root.pageStack.columnView.columnResizeMode === Kirigami.ColumnView.FixedColumns
 
             readonly property real alignSeparatorWidth: root.contentScroll.ScrollBar.vertical.visible ? 15.7 : 14.6
             readonly property real fullWidth: (Kirigami.Units.gridUnit * alignSeparatorWidth) - Kirigami.Units.largeSpacing
@@ -408,7 +418,7 @@ Kirigami.Page {
 
                     ToolButton {
                         icon.name: "go-up"
-                        text: i18n("Previous")
+                        text: KI18n.i18n("Previous")
                         display: AbstractButton.IconOnly
                         onClicked: root.document.findPrevious()
                         enabled: root.document.searchMatchCount > 0
@@ -418,33 +428,33 @@ Kirigami.Page {
                         ToolTip.delay: Kirigami.Units.toolTipDelay
                         Shortcut {
                             sequence: StandardKey.FindPrevious
-                            onActivated: document.findPrevious()
+                            onActivated: root.document.findPrevious()
                         }
                     }
 
                     ToolButton {
                         icon.name: "go-down"
-                        text: i18n("Next")
+                        text: KI18n.i18n("Next")
                         display: AbstractButton.IconOnly
-                        onClicked: document.findNext()
-                        enabled: document.searchMatchCount > 0
+                        onClicked: root.document.findNext()
+                        enabled: root.document.searchMatchCount > 0
 
                         ToolTip.text: text
                         ToolTip.visible: hovered
                         ToolTip.delay: Kirigami.Units.toolTipDelay
                         Shortcut {
                             sequence: StandardKey.FindNext
-                            onActivated: document.findNext()
+                            onActivated: root.document.findNext()
                         }
                     }
 
                     Label {
                         text: {
-                            if (document.searchMatchCount === 0) {
+                            if (root.document.searchMatchCount === 0) {
                                 textArea.deselect();
-                                return i18n("No matches");
+                                return KI18n.i18n("No matches");
                             }
-                            return i18n("%1/%2", document.searchCurrentMatch + 1, document.searchMatchCount);
+                            return KI18n.i18n("%1/%2", root.document.searchCurrentMatch + 1, root.document.searchMatchCount);
                         }
                         Layout.minimumWidth: Kirigami.Units.gridUnit * 4
                     }
@@ -456,14 +466,14 @@ Kirigami.Page {
                     ToolButton {
                         id: replaceToggleButton
                         icon.name: searchBar.isReplaceVisible ? "arrow-up" : "edit-find-replace-symbolic"
-                        text: i18n("Replace")
+                        text: KI18n.i18n("Replace")
                         display: AbstractButton.IconOnly
-                        onClicked: toggleReplace()
+                        onClicked: root.toggleReplace()
 
                         Shortcut {
                             sequence: StandardKey.Replace
                             enabled: root.visible
-                            onActivated: toggleReplace()
+                            onActivated: root.toggleReplace()
                         }
 
                         ToolTip.text: text
@@ -474,7 +484,7 @@ Kirigami.Page {
                     ToolButton {
                         id: closeButton
                         icon.name: "dialog-close"
-                        text: i18n("Close")
+                        text: KI18n.i18n("Close")
                         display: AbstractButton.IconOnly
                         onClicked: {
                             root.closeSearch()
@@ -496,15 +506,15 @@ Kirigami.Page {
                     TextField {
                         id: replaceField
                         Layout.fillWidth: true
-                        placeholderText: i18n("Replace with...")
+                        placeholderText: KI18n.i18n("Replace with...")
                         Keys.onShortcutOverride: (event)=> event.accepted = (event.key === Qt.Key_Escape)
                         Keys.onReturnPressed: (event) => {
                             if (event.modifiers & Qt.ControlModifier) {
-                                const count = document.replaceAll(replaceField.text);
-                                replaceMessage.text = i18ncp("@info:status", "Replaced %1 occurrence", "Replaced %1 occurrences", count);
+                                const count = root.document.replaceAll(replaceField.text);
+                                replaceMessage.text = KI18n.i18ncp("@info:status", "Replaced %1 occurrence", "Replaced %1 occurrences", count);
                                 replaceMessage.visible = true;
                             } else {
-                                document.replaceCurrent(replaceField.text);
+                                root.document.replaceCurrent(replaceField.text);
                             }
 
                             searchField.forceActiveFocus();
@@ -525,10 +535,10 @@ Kirigami.Page {
 
                     ToolButton {
                         icon.name: "document-swap"
-                        text: i18n("Replace")
+                        text: KI18n.i18n("Replace")
                         display: AbstractButton.IconOnly
-                        onClicked: document.replaceCurrent(replaceField.text)
-                        enabled: document.searchMatchCount > 0
+                        onClicked: root.document.replaceCurrent(replaceField.text)
+                        enabled: root.document.searchMatchCount > 0
 
                         ToolTip.text: text
                         ToolTip.visible: hovered
@@ -537,14 +547,14 @@ Kirigami.Page {
 
                     ToolButton {
                         icon.name: "view-refresh"
-                        text: i18n("Replace All")
+                        text: KI18n.i18n("Replace All")
                         display: AbstractButton.IconOnly
                         onClicked: {
-                            const count = document.replaceAll(replaceField.text);
-                            replaceMessage.text = i18ncp("@info:status", "Replaced %1 occurrence", "Replaced %1 occurrences", count);
+                            const count = root.document.replaceAll(replaceField.text);
+                            replaceMessage.text = KI18n.i18ncp("@info:status", "Replaced %1 occurrence", "Replaced %1 occurrences", count);
                             replaceMessage.visible = true;
                         }
-                        enabled: document.searchMatchCount > 0
+                        enabled: root.document.searchMatchCount > 0
 
                         ToolTip.text: text
                         ToolTip.visible: hovered
@@ -553,10 +563,10 @@ Kirigami.Page {
 
                     Label {
                         text: {
-                            if (document.searchMatchCount === 0) {
-                                return i18n("No matches");
+                            if (root.document.searchMatchCount === 0) {
+                                return KI18n.i18n("No matches");
                             }
-                            return i18n("%1/%2", document.searchCurrentMatch + 1, document.searchMatchCount);
+                            return KI18n.i18n("%1/%2", root.document.searchCurrentMatch + 1, root.document.searchMatchCount);
                         }
                         Layout.minimumWidth: Kirigami.Units.gridUnit * 4
                     }
@@ -648,7 +658,7 @@ Kirigami.Page {
             id: textArea
 
             readonly property int marginMultiplier: 6
-            readonly property bool applyWideScreenMargin: (root.isWideScreen && root.pageStack.columnView.columnResizeMode === Kirigami.ColumnView.FixedColumns) || singleDocumentMode
+            readonly property bool applyWideScreenMargin: (root.isWideScreen && root.pageStack.columnView.columnResizeMode === Kirigami.ColumnView.FixedColumns) || root.singleDocumentMode
             textMargin: applyWideScreenMargin ? Kirigami.Units.largeSpacing * marginMultiplier : Kirigami.Units.smallSpacing * marginMultiplier
 
             leftPadding: 0
@@ -723,7 +733,7 @@ Kirigami.Page {
                         return;
                     }
                 } else if (event.matches(StandardKey.Find)) {
-                    toggleSearch();
+                    root.toggleSearch();
                     event.accepted = true;
                     return;
                 }

src/qml/GlobalMenuBar.qml
@@ -4,6 +4,7 @@
 import Qt.labs.platform as Labs
 import org.kde.kirigamiaddons.statefulapp.labs as StatefulAppLabs
 import org.kde.marknote
+import org.kde.ki18n
 
 Labs.MenuBar {
     id: root
@@ -12,10 +13,12 @@ Labs.MenuBar {
 
     NativeEditMenu {}
 
-    NativeWindowMenu {}
+    NativeWindowMenu {
+        _window: root.window
+    }
 
     Labs.Menu {
-        title: i18nc("@action:menu", "Settings")
+        title: KI18n.i18nc("@action:menu", "Settings")
 
         StatefulAppLabs.NativeMenuItem {
             actionName: 'options_configure_keybinding'

src/qml/ImageDialog.qml
@@ -5,6 +5,7 @@ import QtQuick
 import QtCore
 import org.kde.kirigami as Kirigami
 import org.kde.kirigamiaddons.formcard as FormCard
+import org.kde.ki18n
 import QtQuick.Controls as QQC2
 import QtQuick.Layouts
 import QtQuick.Dialogs
@@ -15,16 +16,16 @@ FormCard.FormCardDialog {
 
     property string notePath
 
-    title: i18nc("@title:window", "Insert Image")
+    title: KI18n.i18nc("@title:window", "Insert Image")
     standardButtons: QQC2.Dialog.Ok | QQC2.Dialog.Cancel
 
     FileDialog {
         id: fileDialog
 
-        title: i18nc("@title:window", "Select an image")
+        title: KI18n.i18nc("@title:window", "Select an image")
         currentFolder: StandardPaths.writableLocation(StandardPaths.PicturesLocation)
         fileMode: FileDialog.OpenFile
-        nameFilters: [i18n("Image files (*.jpg *.jpeg *.png *.svg *.webp)"), i18n("All files (*)")]
+        nameFilters: [KI18n.i18n("Image files (*.jpg *.jpeg *.png *.svg *.webp)"), i18n("All files (*)")]
         onAccepted: imageField.path = selectedFile
     }
 
@@ -39,7 +40,7 @@ FormCard.FormCardDialog {
 
         property url path
 
-        text: i18nc("@label:textbox", "Image Location:")
+        text: KI18n.i18nc("@label:textbox", "Image Location:")
         description: path.toString().length > 0 ? path.toString().split('/').slice(-1)[0] : ''
 
         onClicked: fileDialog.open()
@@ -50,7 +51,7 @@ FormCard.FormCardDialog {
     FormCard.FormButtonDelegate {
         id: sketchField
 
-        text: i18nc("@label:textbox", "Quick Sketch")
+        text: KI18n.i18nc("@label:textbox", "Quick Sketch")
         onClicked: {
             sketchDialog.open()
         }

src/qml/LinkDialog.qml
@@ -3,9 +3,8 @@
 
 import QtQuick
 import QtQuick.Controls as QQC2
-import QtQuick.Layouts
 
-import org.kde.kirigami as Kirigami
+import org.kde.ki18n
 import org.kde.kirigamiaddons.formcard as FormCard
 
 FormCard.FormCardDialog {
@@ -14,13 +13,13 @@ FormCard.FormCardDialog {
     property alias linkText: linkTextField.text
     property alias linkUrl: linkUrlField.text
 
-    title: i18nc("@title:window", "Insert Link")
+    title: KI18n.i18nc("@title:window", "Insert Link")
     standardButtons: QQC2.Dialog.Ok | QQC2.Dialog.Cancel
 
     FormCard.FormTextFieldDelegate {
         id: linkTextField
 
-        label: i18nc("@label:textbox", "Link Text:")
+        label: KI18n.i18nc("@label:textbox", "Link Text:")
     }
 
     FormCard.FormDelegateSeparator {}
@@ -28,6 +27,6 @@ FormCard.FormCardDialog {
     FormCard.FormTextFieldDelegate {
         id: linkUrlField
 
-        label: i18nc("@label:textbox", "Link URL:")
+        label: KI18n.i18nc("@label:textbox", "Link URL:")
     }
 }

src/qml/Main.qml
@@ -4,7 +4,6 @@
  *   SPDX-FileCopyrightText: 2026 Valentyn Bondarenko <bondarenko@vivaldi.net>
  */
 
-import QtCore
 import QtQuick
 import QtQuick.Controls as Controls
 import QtQuick.Layouts
@@ -593,7 +592,7 @@ StatetfulApp.StatefulWindow {
                     }
                 }
             }
-            Item { height: Kirigami.Units.largeSpacing * 3 }
+            Item { implicitHeight: Kirigami.Units.largeSpacing * 3 }
         }
     }
 }

src/qml/MainEditor.qml
@@ -3,13 +3,9 @@
     SPDX-FileCopyrightText: 2021 Mathis Brüchert <mbb-mail@gmx.de>
 */
 
-import QtCore
 import QtQuick
 import org.kde.kirigami as Kirigami
-import QtQuick.Controls as Controls
-import QtQuick.Layouts
 import org.kde.marknote
-import org.kde.kirigamiaddons.delegates as Delegates
 
 import "components"
 

commit e76c062925fad26deca31e19ab29ffc14168a2e4

Fix notes list sorting
Got broken in 813b9566aa.

src/qml/NotesPage.qml
@@ -339,6 +339,7 @@ Kirigami.ScrollablePage {
             Item {}
         }
 
+        state: Config.sortBehaviour
         states: [
             State {
                 name: "sort-name"